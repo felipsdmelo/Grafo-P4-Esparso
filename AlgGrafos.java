e15715653d56bcd0b4a06ebfe10900ad89b3cab0
Felipe Melo <56006492+felipsdmelo@users.noreply.github.com>
2021-10-02 20:49:09 -0300

    Corrige chamada de método

--- AlgGrafos.java
+++ AlgGrafos.java
@@ -1,7 +1,7 @@
 public class AlgGrafos {
     public static void main(String[] args) {
         Grafo grafo = new Grafo();
-        grafo.abrirTexto("./myfiles/grafo01.txt");
+        grafo.abrirArquivo("./myfiles/grafo01.txt");
         grafo.imprimirGrafo();
         if (grafo.isP4Esparso())
             System.out.println("\nO grafo é P4-Esparso");
