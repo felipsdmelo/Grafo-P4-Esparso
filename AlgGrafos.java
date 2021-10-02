bdc7826947be4695ed815e336dcf86efae2e7adf
Felipe Melo <56006492+felipsdmelo@users.noreply.github.com>
2021-10-02 14:10:13 -0300

    Atualiza arquivo principal com mensagens na tela

--- AlgGrafos.java
+++ AlgGrafos.java
@@ -1,6 +1,11 @@
 public class AlgGrafos {
     public static void main(String[] args) {
         Grafo grafo = new Grafo();
-        grafo.abrirTexto("/myfiles/grafo01.txt");
+        grafo.abrirTexto("./myfiles/true.txt");
+        grafo.imprimirGrafo();
+        if (grafo.isP4Esparso())
+            System.out.println("\nO grafo é P4-Esparso");
+        else
+            System.out.println("\nO grafo não é P4-Esparso");
     }
 }
